9af76a9b8de5af7974ef749120708f00e73411c6
Diegozxp <diegmascarenha@gmail.com>
2020-07-10 17:13:52 +0000

    Segundo projeto
    Signed-off-by: Diegozxp <diegmascarenha@gmail.com>

--- Basico/components/BoasVindas.js
+++ Basico/components/BoasVindas.js
@@ -1,20 +1,62 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
 
 function BoasVindas(props) {
 
   const [mensagem, setMensagem] = useState('Seja bem-vindo(a)!');
+    const [name, setName] = useState('');
+    const [subname, setSubname] = useState('');
+
+    const [mostrarMensagem, setMostrarMensagem] = useState(false);
+
+    function mostrarSaudacao() {
+        if(name !== '' && subname !== ''){
+            setMostrarMensagem(true);
+        }else{
+            Alert.alert([ 'Favor preencher.',
+                         'Os campos não podem ficar vazios.'
+            ],           {text: 'OK'});
+        }
+    }
+
+    function clear() {
+        setName('');
+        setSubname('');
+        setMostrarMensagem(false);
+    }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Olá {props.saudacao}</Text>
-      <Text style={styles.texto}>{mensagem}</Text>
+        <TextInput 
+            placeholder="Nome:"
+            style={styles.input}
+            value={name}
+            onChangeText={value => setName(value)} 
+        />
+        <TextInput 
+            placeholder="Sobrenome:"
+            style={styles.input}
+            value={subname}
+            onChangeText={value => setSubname(value)} 
+        />
+        <View style={styles.buttonContainer}>
+            <Button onPress={mostrarSaudacao} color="#10ad10" title="ok"  />
+        </View>
+        <View>
+         <Button onPress={clear} color="#10ad10" title="limpar"  />
+        </View>
+      {mostrarMensagem &&(
+      <>
+         <Text style={styles.texto}>Olá {name} {subname} {props.saudacao}</Text>
+         <Text style={styles.texto}>{mensagem}</Text>
+      </>
+ )}
     </View>
   )
 }
 const styles = StyleSheet.create({
   container: {
-     backgroundColor: '#081a31',
+     backgroundColor: '#000000',
      width: 300,
      borderRadius: 5,
      padding: 10,
@@ -25,5 +67,17 @@ const styles = StyleSheet.create({
     color: '#FFFFFF',
     textAlign: "center",
   },
+  input: {
+    borderColor: '#fff',
+    fontSize: 18,  
+    height: 40,
+      padding: 5,
+      backgroundColor: '#fff',
+      borderRadius: 8, 
+      marginBottom: 20,  
+  },
+  buttonContainer: {
+      marginBottom: 10,
+  } 
 });
 export default BoasVindas;
